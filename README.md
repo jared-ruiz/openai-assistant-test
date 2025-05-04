086b92081d00e453c60037be0bc401e98dd3256f
Jared Ruiz <jared707@gmail.com>
2025-05-03 21:59:01 -0500

    Ended a full day of work and got a simple get request to an open API working, returning article information and removing html elements from the output to then return as plain text. I have a long ways to go to satisfy the requirements but I am ramping up quick.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # **OptiSigns-Backend-Test** - Jared Ruiz
 
-### This project was a ground up, 0-100 learning experince and I documented everything I learned and how I approached my probelm solving here if you would like to read through my logs!
+> This project was a ground up, 0-100 learning experince and I documented everything I learned and how I approached my probelm solving here if you would like to read through my logs!
 
-[Learning Log: Jared Ruiz](./learning_log.md);
+[Learning Log: Jared Ruiz](./learning_log.md)
 
 ## Installation Instructions (coming soon)
--- webscraper.py
+++ webscraper.py
@@ -2,12 +2,41 @@ from bs4 import BeautifulSoup
 import requests
 import markdownify
 
-# page to test scrape [https://quotes.toscrape.com/]
-page_to_scrape = requests.get("https://quotes.toscrape.com/")
-soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+# create plain text of soup data
+def html_to_text(html_text):
+    soup = BeautifulSoup(html_text, "html.parser")
+    return soup.get_text(strip=True)
 
-quotes = soup.find_all("span", attrs={"class": "text"})
-authors = soup.find_all("small", attrs={"class": "author"})
+# return article data from open API endpoint
+def get_articles(): 
+    
+    # use endpoint in zendesk article page for retrieving articles
+    url = "https://support.optisigns.com/api/v2/help_center/articles.json"
+    
+    try:
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            articles = response.json()
+            return articles
+        else:
+            print("Error:", response.status_code)
+            return None
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return None
+    
+def main():
+    #returns dictionary to then parse through each article and it's key value paies
+    item = get_articles()
 
-for quote, author in zip(quotes, authors):
-    print(quote.text + " - " + author.text)
+    # this checks to see if there is actually a dictionary returned and it contains the article key too
+    if item and "articles" in item:
+        for article in item["articles"]:
+            article_html_body = article["body"]
+            article_body = html_to_text(article_html_body)
+            print(article_body)
+    else:
+        print("No articles found")
+    
+main()
